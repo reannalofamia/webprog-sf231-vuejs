81a82568ece86a322f28e7ddebc6d93a4dbade79
reannalofamia <rplofamia@student.apc.edu.ph>
2025-01-23 14:49:39 +0800

    updates

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,23 @@
 <script></script>
 
-<template> 
-
-<h1>Hello WEBPROG SF231 World!</h1>
-
+<template>
+    <h1>Food</h1>
+    <food-item />
+    <food-item />
+    <food-item />
 </template>
 
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    width: 120px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+  #app > div:hover {
+    cursor: pointer;
+  }
+</style>
+
