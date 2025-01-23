d7cbe3c512e7a0726e26b613bb437bffda94e53f
reannalofamia <rplofamia@student.apc.edu.ph>
2025-01-23 15:09:08 +0800

    vertical

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,11 @@
 <script></script>
 
 <template>
-    <h1>Food</h1>
-    <food-item />
-    <food-item2 />
-    <food-item />
+   <h1>Food</h1>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/>  <br>
+  <food-item2/> <br>
 </template>
 
 <style></style>
